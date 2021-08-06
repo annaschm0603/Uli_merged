81dda9a36dc5c848175797c3e3267af88344d2e2
annaschm0603 <41060216+annaschm0603@users.noreply.github.com>
2021-08-06 13:37:19 +0200

    edited the overlapping part so that it only contains overlap and not the wt specific as well

--- Untitled-1.sh
+++ Untitled-1.sh
@@ -18,52 +18,52 @@ ml bedtools/2.27.1-foss-2018b
 
 #####ISWI specific
 ###W6
-bedtools intersect -v -a macs2_peaks/Iswi_H2A.W6/broad_Iswi_H2A.W6.merged_peaks.broadPeak -b macs2_peaks/WT_H2A.W6/broad_WT_H2A.W6.merged_peaks.broadPeak -wao -e -f 0.5 -F 0.5 > test_intersect/W6_broad_aIswi_bWT_iswispecific_05.bed
-bedtools intersect -v -a macs2_peaks/Iswi_H2A.W6/summit_Iswi_H2A.W6.merged_peaks.narrowPeak -b macs2_peaks/WT_H2A.W6/summit_WT_H2A.W6.merged_peaks.narrowPeak -wao -e -f 0.5 -F 0.5 > test_intersect/W6_narrow_aIswi_bWT_iswispecific_05.bed
+bedtools intersect -v -a macs2_peaks/Iswi_H2A.W6/broad_Iswi_H2A.W6.merged_peaks.broadPeak -b macs2_peaks/WT_H2A.W6/broad_WT_H2A.W6.merged_peaks.broadPeak -e -f 0.5 -F 0.5 > test_intersect/W6_broad_aIswi_bWT_iswispecific_05.bed
+bedtools intersect -v -a macs2_peaks/Iswi_H2A.W6/summit_Iswi_H2A.W6.merged_peaks.narrowPeak -b macs2_peaks/WT_H2A.W6/summit_WT_H2A.W6.merged_peaks.narrowPeak -e -f 0.5 -F 0.5 > test_intersect/W6_narrow_aIswi_bWT_iswispecific_05.bed
 ###W7
-bedtools intersect -v -a macs2_peaks/Iswi_H2A.W7/broad_Iswi_H2A.W7.merged_peaks.broadPeak -b macs2_peaks/WT_H2A.W7/broad_WT_H2A.W7.merged_peaks.broadPeak -wao -e -f 0.5 -F 0.5 > test_intersect/W7_broad_aIswi_bWT_iswispecific_05.bed
-bedtools intersect -v -a macs2_peaks/Iswi_H2A.W7/summit_Iswi_H2A.W7.merged_peaks.narrowPeak -b macs2_peaks/WT_H2A.W7/summit_WT_H2A.W7.merged_peaks.narrowPeak -wao -e -f 0.5 -F 0.5 > test_intersect/W7_narrow_aIswi_bWT_iswispecific_05.bed
+bedtools intersect -v -a macs2_peaks/Iswi_H2A.W7/broad_Iswi_H2A.W7.merged_peaks.broadPeak -b macs2_peaks/WT_H2A.W7/broad_WT_H2A.W7.merged_peaks.broadPeak -e -f 0.5 -F 0.5 > test_intersect/W7_broad_aIswi_bWT_iswispecific_05.bed
+bedtools intersect -v -a macs2_peaks/Iswi_H2A.W7/summit_Iswi_H2A.W7.merged_peaks.narrowPeak -b macs2_peaks/WT_H2A.W7/summit_WT_H2A.W7.merged_peaks.narrowPeak -e -f 0.5 -F 0.5 > test_intersect/W7_narrow_aIswi_bWT_iswispecific_05.bed
 ###X
-bedtools intersect -v -a macs2_peaks/Iswi_H2A.X/broad_Iswi_H2A.X.merged_peaks.broadPeak -b macs2_peaks/WT_H2A.X/broad_WT_H2A.X.merged_peaks.broadPeak -wao -e -f 0.5 -F 0.5 > test_intersect/X_broad_aIswi_bWT_iswispecific_05.bed
-bedtools intersect -v -a macs2_peaks/Iswi_H2A.X/summit_Iswi_H2A.X.merged_peaks.narrowPeak -b macs2_peaks/WT_H2A.X/summit_WT_H2A.X.merged_peaks.narrowPeak -wao -e -f 0.5 -F 0.5 > test_intersect/X_narrow_aIswi_bWT_iswispecific_05.bed
+bedtools intersect -v -a macs2_peaks/Iswi_H2A.X/broad_Iswi_H2A.X.merged_peaks.broadPeak -b macs2_peaks/WT_H2A.X/broad_WT_H2A.X.merged_peaks.broadPeak -e -f 0.5 -F 0.5 > test_intersect/X_broad_aIswi_bWT_iswispecific_05.bed
+bedtools intersect -v -a macs2_peaks/Iswi_H2A.X/summit_Iswi_H2A.X.merged_peaks.narrowPeak -b macs2_peaks/WT_H2A.X/summit_WT_H2A.X.merged_peaks.narrowPeak -e -f 0.5 -F 0.5 > test_intersect/X_narrow_aIswi_bWT_iswispecific_05.bed
 ###Z
-bedtools intersect -v -a macs2_peaks/Iswi_H2A.Z/broad_Iswi_H2A.Z.merged_peaks.broadPeak -b macs2_peaks/WT_H2A.Z/broad_WT_H2A.Z.merged_peaks.broadPeak -wao -e -f 0.5 -F 0.5 > test_intersect/Z_broad_aIswi_bWT_iswispecific_05.bed
-bedtools intersect -v -a macs2_peaks/Iswi_H2A.Z/summit_Iswi_H2A.Z.merged_peaks.narrowPeak -b macs2_peaks/WT_H2A.Z/summit_WT_H2A.Z.merged_peaks.narrowPeak -wao -e -f 0.5 -F 0.5 > test_intersect/Z_narrow_aIswi_bWT_iswispecific_05.bed
+bedtools intersect -v -a macs2_peaks/Iswi_H2A.Z/broad_Iswi_H2A.Z.merged_peaks.broadPeak -b macs2_peaks/WT_H2A.Z/broad_WT_H2A.Z.merged_peaks.broadPeak -e -f 0.5 -F 0.5 > test_intersect/Z_broad_aIswi_bWT_iswispecific_05.bed
+bedtools intersect -v -a macs2_peaks/Iswi_H2A.Z/summit_Iswi_H2A.Z.merged_peaks.narrowPeak -b macs2_peaks/WT_H2A.Z/summit_WT_H2A.Z.merged_peaks.narrowPeak -e -f 0.5 -F 0.5 > test_intersect/Z_narrow_aIswi_bWT_iswispecific_05.bed
 ###H2A.13
-bedtools intersect -v -a macs2_peaks/Iswi_H2A.13/broad_Iswi_H2A.13.merged_peaks.broadPeak -b macs2_peaks/WT_H2A.13/broad_WT_H2A.13.merged_peaks.broadPeak -wao -e -f 0.5 -F 0.5 > test_intersect/H2A.13_broad_aIswi_bWT_iswispecific_05.bed
-bedtools intersect -v -a macs2_peaks/Iswi_H2A.13/summit_Iswi_H2A.13.merged_peaks.narrowPeak -b macs2_peaks/WT_H2A.13/summit_WT_H2A.13.merged_peaks.narrowPeak -wao -e -f 0.5 -F 0.5 > test_intersect/H2A.13_narrow_aIswi_bWT_iswispecific_05.bed
+bedtools intersect -v -a macs2_peaks/Iswi_H2A.13/broad_Iswi_H2A.13.merged_peaks.broadPeak -b macs2_peaks/WT_H2A.13/broad_WT_H2A.13.merged_peaks.broadPeak -e -f 0.5 -F 0.5 > test_intersect/H2A.13_broad_aIswi_bWT_iswispecific_05.bed
+bedtools intersect -v -a macs2_peaks/Iswi_H2A.13/summit_Iswi_H2A.13.merged_peaks.narrowPeak -b macs2_peaks/WT_H2A.13/summit_WT_H2A.13.merged_peaks.narrowPeak -e -f 0.5 -F 0.5 > test_intersect/H2A.13_narrow_aIswi_bWT_iswispecific_05.bed
 
 
 #####overlap between wt and ISWI
 ###W6
-bedtools intersect -a macs2_peaks/WT_H2A.W6/summit_WT_H2A.W6.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.W6/summit_Iswi_H2A.W6.merged_peaks.narrowPeak -wao -e -f 0.5 -F 0.5 > test_intersect/W6_narrow_aWT_bIswi_overlap_05.bed
-bedtools intersect -a macs2_peaks/WT_H2A.W6/broad_WT_H2A.W6.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.W6/broad_Iswi_H2A.W6.merged_peaks.broadPeak -wao -e -f 0.5 -F 0.5 > test_intersect/W6_broad_aWT_bIswi_overlap_05.bed
+bedtools intersect -a macs2_peaks/WT_H2A.W6/summit_WT_H2A.W6.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.W6/summit_Iswi_H2A.W6.merged_peaks.narrowPeak -wa -wb -e -f 0.5 -F 0.5 | bedtools merge > test_intersect/W6_narrow_aWT_bIswi_overlap_05.bed 
+bedtools intersect -a macs2_peaks/WT_H2A.W6/broad_WT_H2A.W6.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.W6/broad_Iswi_H2A.W6.merged_peaks.broadPeak -wa -wb -e -f 0.5 -F 0.5 | bedtools merge > test_intersect/W6_broad_aWT_bIswi_overlap_05.bed
 ###W7
-bedtools intersect -a macs2_peaks/WT_H2A.W7/summit_WT_H2A.W7.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.W7/summit_Iswi_H2A.W7.merged_peaks.narrowPeak -wao -e -f 0.5 -F 0.5 > test_intersect/W7_narrow_aWT_bIswi_overlap_05.bed
-bedtools intersect -a macs2_peaks/WT_H2A.W7/broad_WT_H2A.W7.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.W7/broad_Iswi_H2A.W7.merged_peaks.broadPeak -wao -e -f 0.5 -F 0.5 > test_intersect/W7_broad_aWT_bIswi_overlap_05.bed
+bedtools intersect -a macs2_peaks/WT_H2A.W7/summit_WT_H2A.W7.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.W7/summit_Iswi_H2A.W7.merged_peaks.narrowPeak -wa -wb -e -f 0.5 -F 0.5 | bedtools merge > test_intersect/W7_narrow_aWT_bIswi_overlap_05.bed 
+bedtools intersect -a macs2_peaks/WT_H2A.W7/broad_WT_H2A.W7.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.W7/broad_Iswi_H2A.W7.merged_peaks.broadPeak -wa -wb -e -f 0.5 -F 0.5 | bedtools merge > test_intersect/W7_broad_aWT_bIswi_overlap_05.bed
 ###X
-bedtools intersect -a macs2_peaks/WT_H2A.X/summit_WT_H2A.X.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.X/summit_Iswi_H2A.X.merged_peaks.narrowPeak -wao -e -f 0.5 -F 0.5 > test_intersect/X_narrow_aWT_bIswi_overlap_05.bed
-bedtools intersect -a macs2_peaks/WT_H2A.X/broad_WT_H2A.X.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.X/broad_Iswi_H2A.X.merged_peaks.broadPeak -wao -e -f 0.5 -F 0.5 > test_intersect/X_broad_aWT_bIswi_overlap_05.bed
+bedtools intersect -a macs2_peaks/WT_H2A.X/summit_WT_H2A.X.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.X/summit_Iswi_H2A.X.merged_peaks.narrowPeak -wa -wb -e -f 0.5 -F 0.5 | bedtools merge > test_intersect/X_narrow_aWT_bIswi_overlap_05.bed 
+bedtools intersect -a macs2_peaks/WT_H2A.X/broad_WT_H2A.X.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.X/broad_Iswi_H2A.X.merged_peaks.broadPeak -wa -wb -e -f 0.5 -F 0.5 | bedtools merge > test_intersect/X_broad_aWT_bIswi_overlap_05.bed
 ###Z
-bedtools intersect -a macs2_peaks/WT_H2A.Z/summit_WT_H2A.Z.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.Z/summit_Iswi_H2A.Z.merged_peaks.narrowPeak -wao -e -f 0.5 -F 0.5 > test_intersect/Z_narrow_aWT_bIswi_overlap_05.bed
-bedtools intersect -a macs2_peaks/WT_H2A.Z/broad_WT_H2A.Z.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.Z/broad_Iswi_H2A.Z.merged_peaks.broadPeak -wao -e -f 0.5 -F 0.5 > test_intersect/Z_broad_aWT_bIswi_overlap_05.bed
+bedtools intersect -a macs2_peaks/WT_H2A.Z/summit_WT_H2A.Z.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.Z/summit_Iswi_H2A.Z.merged_peaks.narrowPeak -wa -wb -e -f 0.5 -F 0.5 | bedtools merge > test_intersect/Z_narrow_aWT_bIswi_overlap_05.bed 
+bedtools intersect -a macs2_peaks/WT_H2A.Z/broad_WT_H2A.Z.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.Z/broad_Iswi_H2A.Z.merged_peaks.broadPeak -wa -wb -e -f 0.5 -F 0.5 | bedtools merge > test_intersect/Z_broad_aWT_bIswi_overlap_05.bed
 ###H2A.13
-bedtools intersect -a macs2_peaks/WT_H2A.13/summit_WT_H2A.13.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.13/summit_Iswi_H2A.13.merged_peaks.narrowPeak -wao -e -f 0.5 -F 0.5 > test_intersect/H2A.13_narrow_aWT_bIswi_overlap_05.bed
-bedtools intersect -a macs2_peaks/WT_H2A.13/broad_WT_H2A.13.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.13/broad_Iswi_H2A.13.merged_peaks.broadPeak -wao -e -f 0.5 -F 0.5 > test_intersect/H2A.13_broad_aWT_bIswi_overlap_05.bed
+bedtools intersect -a macs2_peaks/WT_H2A.13/summit_WT_H2A.13.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.13/summit_Iswi_H2A.13.merged_peaks.narrowPeak -wa -wb -e -f 0.5 -F 0.5 | bedtools merge > test_intersect/H2A.13_narrow_aWT_bIswi_overlap_05.bed 
+bedtools intersect -a macs2_peaks/WT_H2A.13/broad_WT_H2A.13.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.13/broad_Iswi_H2A.13.merged_peaks.broadPeak -wa -wb -e -f 0.5 -F 0.5 | bedtools merge > test_intersect/H2A.13_broad_aWT_bIswi_overlap_05.bed
 
 #####WT specific
 ###W6
-bedtools intersect -v -a macs2_peaks/WT_H2A.W6/summit_WT_H2A.W6.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.W6/summit_Iswi_H2A.W6.merged_peaks.narrowPeak -wao -e -f 0.5 -F 0.5 > test_intersect/W6_narrow_aWT_bIswi_wtspecific_05.bed
-bedtools intersect -v -a macs2_peaks/WT_H2A.W6/broad_WT_H2A.W6.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.W6/broad_Iswi_H2A.W6.merged_peaks.broadPeak -wao -e -f 0.5 -F 0.5 > test_intersect/W6_broad_aWT_bIswi_wtspecific_05.bed
+bedtools intersect -v -a macs2_peaks/WT_H2A.W6/summit_WT_H2A.W6.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.W6/summit_Iswi_H2A.W6.merged_peaks.narrowPeak -e -f 0.5 -F 0.5 > test_intersect/W6_narrow_aWT_bIswi_wtspecific_05.bed
+bedtools intersect -v -a macs2_peaks/WT_H2A.W6/broad_WT_H2A.W6.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.W6/broad_Iswi_H2A.W6.merged_peaks.broadPeak -e -f 0.5 -F 0.5 > test_intersect/W6_broad_aWT_bIswi_wtspecific_05.bed
 ###W7
-bedtools intersect -v -a macs2_peaks/WT_H2A.W7/summit_WT_H2A.W7.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.W7/summit_Iswi_H2A.W7.merged_peaks.narrowPeak -wao -e -f 0.5 -F 0.5 > test_intersect/W7_narrow_aWT_bIswi_wtspecific_05.bed
-bedtools intersect -v -a macs2_peaks/WT_H2A.W7/broad_WT_H2A.W7.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.W7/broad_Iswi_H2A.W7.merged_peaks.broadPeak -wao -e -f 0.5 -F 0.5 > test_intersect/W7_broad_aWT_bIswi_wtspecific_05.bed
+bedtools intersect -v -a macs2_peaks/WT_H2A.W7/summit_WT_H2A.W7.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.W7/summit_Iswi_H2A.W7.merged_peaks.narrowPeak -e -f 0.5 -F 0.5 > test_intersect/W7_narrow_aWT_bIswi_wtspecific_05.bed
+bedtools intersect -v -a macs2_peaks/WT_H2A.W7/broad_WT_H2A.W7.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.W7/broad_Iswi_H2A.W7.merged_peaks.broadPeak -e -f 0.5 -F 0.5 > test_intersect/W7_broad_aWT_bIswi_wtspecific_05.bed
 ###X
-bedtools intersect -v -a macs2_peaks/WT_H2A.X/summit_WT_H2A.X.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.X/summit_Iswi_H2A.X.merged_peaks.narrowPeak -wao -e -f 0.5 -F 0.5 > test_intersect/X_narrow_aWT_bIswi_wtspecific_05.bed
-bedtools intersect -v -a macs2_peaks/WT_H2A.X/broad_WT_H2A.X.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.X/broad_Iswi_H2A.X.merged_peaks.broadPeak -wao -e -f 0.5 -F 0.5 > test_intersect/X_broad_aWT_bIswi_wtspecific_05.bed
+bedtools intersect -v -a macs2_peaks/WT_H2A.X/summit_WT_H2A.X.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.X/summit_Iswi_H2A.X.merged_peaks.narrowPeak -e -f 0.5 -F 0.5 > test_intersect/X_narrow_aWT_bIswi_wtspecific_05.bed
+bedtools intersect -v -a macs2_peaks/WT_H2A.X/broad_WT_H2A.X.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.X/broad_Iswi_H2A.X.merged_peaks.broadPeak  -e -f 0.5 -F 0.5 > test_intersect/X_broad_aWT_bIswi_wtspecific_05.bed
 ###Z
-bedtools intersect -v -a macs2_peaks/WT_H2A.Z/summit_WT_H2A.Z.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.Z/summit_Iswi_H2A.Z.merged_peaks.narrowPeak -wao -e -f 0.5 -F 0.5 > test_intersect/Z_narrow_aWT_bIswi_wtspecific_05.bed
-bedtools intersect -v -a macs2_peaks/WT_H2A.Z/broad_WT_H2A.Z.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.Z/broad_Iswi_H2A.Z.merged_peaks.broadPeak -wao -e -f 0.5 -F 0.5 > test_intersect/Z_broad_aWT_bIswi_wtspecific_05.bed
+bedtools intersect -v -a macs2_peaks/WT_H2A.Z/summit_WT_H2A.Z.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.Z/summit_Iswi_H2A.Z.merged_peaks.narrowPeak  -e -f 0.5 -F 0.5 > test_intersect/Z_narrow_aWT_bIswi_wtspecific_05.bed
+bedtools intersect -v -a macs2_peaks/WT_H2A.Z/broad_WT_H2A.Z.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.Z/broad_Iswi_H2A.Z.merged_peaks.broadPeak -e -f 0.5 -F 0.5 > test_intersect/Z_broad_aWT_bIswi_wtspecific_05.bed
 ###H2A.13
-bedtools intersect -v -a macs2_peaks/WT_H2A.13/summit_WT_H2A.13.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.13/summit_Iswi_H2A.13.merged_peaks.narrowPeak -wao -e -f 0.5 -F 0.5 > test_intersect/H2A.13_narrow_aWT_bIswi_wtspecific_05.bed
-bedtools intersect -v -a macs2_peaks/WT_H2A.13/broad_WT_H2A.13.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.13/broad_Iswi_H2A.13.merged_peaks.broadPeak -wao -e -f 0.5 -F 0.5 > test_intersect/H2A.13_broad_aWT_bIswi_wtspecific_05.bed
+bedtools intersect -v -a macs2_peaks/WT_H2A.13/summit_WT_H2A.13.merged_peaks.narrowPeak -b macs2_peaks/Iswi_H2A.13/summit_Iswi_H2A.13.merged_peaks.narrowPeak -e -f 0.5 -F 0.5 > test_intersect/H2A.13_narrow_aWT_bIswi_wtspecific_05.bed
+bedtools intersect -v -a macs2_peaks/WT_H2A.13/broad_WT_H2A.13.merged_peaks.broadPeak -b macs2_peaks/Iswi_H2A.13/broad_Iswi_H2A.13.merged_peaks.broadPeak -e -f 0.5 -F 0.5 > test_intersect/H2A.13_broad_aWT_bIswi_wtspecific_05.bed
